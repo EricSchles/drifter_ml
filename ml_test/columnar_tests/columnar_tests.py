27322a1af1f8e9405bb8938967133945bd41b286
Eric Schles <erschles@microsoft.com>
2019-03-12 11:31:56 -0400

    type: refactor * adjusted columnar tests

--- ml_test/columnar_tests/columnar_tests.py
+++ ml_test/columnar_tests/columnar_tests.py
@@ -5,7 +5,6 @@ import numpy as np
 import time
 from sklearn import neighbors
 from scipy import stats
-from sklearn.model_selection import cross_val_score
 
 class DataSanitization(): 
     def __init__(self, data):
@@ -20,39 +19,34 @@ class DataSanitization():
     def is_unique(self, column):
         return len(self.data[column].unique())/len(self.data) == 1
 
-    def has_uniqueness(column, threshold):
+    def has_uniqueness(self, column, threshold):
         return len(self.data[column].unique())/len(self.data) > threshold
 
-    def is_in_range(column, lower_bound, upper_bound, threshold):
+    def is_in_range(self, column, lower_bound, upper_bound, threshold):
         return self.data[(self.data[column] <= upper_bound) & (self.data[column] >= lower_bound)]/len(self.data) > threshold
 
-    def is_non_negative(column):
+    def is_non_negative(self, column):
         return self.data[self.data[column] > 0]
 
-    def is_less_than(column_one, column_two):
+    def is_less_than(self, column_one, column_two):
         return self.data[self.data[column_one] < self.data[column_two]].all()
 
 class ColumnarData():
-    def __init__(self,
-                 historical_data,
-                 new_data,
-                 column_names):
+    def __init__(self, historical_data, new_data):
         self.new_data = new_data
         self.historical_data = historical_data
         
-                 
-    def similiar_correlation(correlation_lower_bound, new_data, historical_data, column_names, pvalue_threshold=0.05):
-        for column_name in column_names:
-            correlation_info = stats.spearmanr(new_data[column_name], historical_data[column_name])
-            if correlation_info.pvalue > pvalue_threshold:
-                return False
-            if correlation_info.correlation < correlation_lower_bound:
-                return False
+    def similiar_correlation(self, column, correlation_lower_bound, pvalue_threshold=0.05):
+        correlation_info = stats.spearmanr(self.new_data[column_name], self.historical_data[column_name])
+        if correlation_info.pvalue > pvalue_threshold:
+            return False
+        if correlation_info.correlation < correlation_lower_bound:
+            return False
         return True
 
-    def similiar_distribution(new_data, historical_data, column_names, pvalue_threshold=0.05):
-        for column_name in column_names:
-            distribution_info = stats.ks_2samp(new_data[column_name], historical_data[column_name])
-            if correlation_info.pvalue < pvalue_threshold:
-                return False
+    def similiar_distribution(self, column, pvalue_threshold=0.05):
+        distribution_info = stats.ks_2samp(self.new_data[column_name],
+                                           self.historical_data[column_name])
+        if distribution_info.pvalue < pvalue_threshold:
+            return False
         return True
