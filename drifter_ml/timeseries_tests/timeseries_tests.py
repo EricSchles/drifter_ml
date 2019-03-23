88d643b6b53883e72a788546a82f4af95fed6bcf
Eric Schles <erschles@microsoft.com>
2019-03-23 08:49:22 -0400

    added time series tests for series

--- drifter_ml/timeseries_tests/timeseries_tests.py
+++ drifter_ml/timeseries_tests/timeseries_tests.py
@@ -1,94 +1,92 @@
 #from mlxtend.evaluate import permutation_test
-from statsmodels.stats import diagnostic
+from statsmodels.tsa import stattools
+from statsmodels.stats import diagnstic
 import warnings
-import pandas as pd
-from statsmodels.tsa.arima_model import ARIMA
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
 warnings.filterwarnings("ignore")
 
 class TimeSeriesTests:
-    def __init__(self, timeseries, ar_values, d_values, ma_values, is_date=False, is_datetime=False, is_time=False):
-        self.timeseries = timeseries
-        if is_date:
-            self.timeseries.index = pd.to_datetime(timeseries["date"])
-            self.timeseries.drop("date", inplace=True)
-        if is_datetime:
-            self.timeseries.index = pd.to_datetime(timeseries["datetime"])
-            self.timeseries.drop("datetime", inplace=True)
-        if is_time:
-            self.timeseries.index = timeseries["time"]
-            self.timeseries.drop("time", inplace=True)
-        self.ar_values = ar_values
-        self.d_values = d_values
-        self.ma_values = ma_values
-        self.model = self._evaluate_models()
+    def __init__(self, data):
+        self.data = data
+
+    def unit_root(self, column, pvalue_tolerance=0.05):
+        """
+        Unit root test, tests whether or not your time series is
+        stationary.  The null hypothesis is that it is not.
+        The alternative hypothesis is that it is.  So if the pvalue is
+        less than the threshold, the series is stationary
         
-    # evaluate an ARIMA model for a given order (p,d,q)
-    def _evaluate_arima_model(self, X, arima_order):
-        # prepare training dataset
-        train, test, _, _ = train_test_split(X, np.zeros(X.shape[0]))
-        history = list(train)
-        # make predictions
-        predictions = list()
-        for t in range(len(test)):
-            model = ARIMA(history, order=arima_order)
-            model_fit = model.fit(disp=0)
-            yhat = model_fit.forecast()[0]
-            predictions.append(yhat)
-            history.append(test[t])
-        # calculate out of sample error
-        error = mean_squared_error(test, predictions)
-        return error
+        if True then stationary
+        if False than not stationary
+        """
+        result = stattools.adfuller(self.data[column])
+        if result[1] < pvalue_tolerance:
+            return True
+        return False
 
-    # evaluate combinations of p, d and q values for an ARIMA model
-    def _evaluate_models(self):
-        dataset = self.timeseries.astype('float32')
-        best_score, best_cfg = float("inf"), None
-        for p in p_values:
-            for d in d_values:
-                for q in q_values:
-                    order = (p,d,q)
-                    try:
-                        mse = evaluate_arima_model(dataset, order)
-                        if mse < best_score:
-                            best_score = mse
-                            best_order = order
-                    except:
-                        continue
-        model = ARIMA(dateset, order=best_order)
-        model_fit = model.fit(disp=0)
-        return model_fit
+    def kpss(self, column, pvalue_tolerance=0.05):
+        """
+        KPSS also tests for stationarity, here the null hypothesis
+        is that the series is stationary, so a pvalue above the tolerance level
+        means it's stationary.  A pvalue below means the alternative hypothesis
+        is true and it is not.
 
-    def acorr_breusch_godfrey(self, nlags=None):
-        return diagnostic.acorr_breusch_godfrey(self.model, nlags=nlags)
+        if True then stationary
+        if False than not stationary
+        """
+        result = stattools.kpss(self.data[column])
+        if result[1] > pvalue_tolerance:
+            return True
+        return False
 
-    def unitroot_adf(self, maxlag=None, trendorder=0, autolag='AIC', store=False):
-        return diagnostic.unitroot_adf(self.timeseries,
-                                       maxlag=maxlag,
-                                       trendorder=trendorder,
-                                       autolag=autolag,
-                                       store=store)
-# evaluate parameters
-p_values = [0, 1, 2, 4, 6, 8, 10]
-d_values = range(0, 3)
-q_values = range(0, 3)
+    def coint(self, column_one, column_two, pvalue_tolerance=0.05):
+        """
+        tests whether or not two series are cointegrated.
+        Cointegration is similar to correlation, because there
+        is a dependence relationship in two random series
+        
+        if True than column_one not cointegrated with column_two
+        if False than column_one cointegrated with column_two
+        """
+        result = stattools.coint(self.data[column_one], self.data[column_two])
+        if result[1] > pvalue_tolerance:
+            return True
+        return False
+        
+    def bds(self, column_one, pvalue_tolerance=0.05):
+        """
+        tests if the column is independent and identically distributed
+        
+        if True variable is iid
+        if False variable is not
+        """
+        result = stattools.bds(self.data[column_one])
+        if result[1] > pvalue_tolerance:
+            return True
+        return False
 
+    def q_stat(self, column_one, pvalue_tolerance=0.05):
+        """
+        tests for auto correlation in the series, this is similiar to
+        serial correlation, as outlied here: 
+        http://gauss.stat.su.se/gu/e/slides/Lectures%208-13/Autocorrelation.pdf
+        
+        the null hypothesis is no autocorrelation
+        the alternative hypothesis is autocorrelation exists.
+        
+        if True variable is not autocorrelated
+        if False variable is autocorrelated
+        """
+        autocorrelation_coefs = stattools.acf(self.data[column_one])
+        result = stattools.q_stat(autocorrelation_coefs)
+        if result[1] > pvalue_tolerance:
+            return True
+        return False
 
-https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.unitroot_adf.html
-https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.het_arch.html
-https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.acorr_breusch_godfrey.html
-https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.acorr_ljungbox.html
-https://www.statsmodels.org/dev/generated/statsmodels.stats.stattools.durbin_watson.html
+    
+# ToDo:
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.unitroot_adf.html
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.het_arch.html
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.acorr_breusch_godfrey.html
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.diagnostic.acorr_ljungbox.html
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.stattools.durbin_watson.html
 
-# x = np.array([1, 2, 3, 4, 5, 6])
-# y = np.array([2, 4, 1, 5, 6, 7])
-
-# print('Observed pearson R: %.2f' % np.corrcoef(x, y)[1][0])
-
-
-# p_value = permutation_test(x, y,
-#                            method='exact',
-#                            func=lambda x, y: np.corrcoef(x, y)[1][0],
-#                            seed=0)
-# print('P value: %.2f' % p_value)
