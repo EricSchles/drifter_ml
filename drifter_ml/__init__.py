cdfda44b21a16eaa24557db303898a6eb791b12e
Eric Schles <ericschles@gmail.com>
2019-03-25 20:40:31 -0400

    updating structure of tests

--- drifter_ml/__init__.py
+++ drifter_ml/__init__.py
@@ -4,7 +4,6 @@ from .classification_tests import classification_tests
 from .columnar_tests import columnar_tests
 from .regression_tests import regression_tests
 from .structural_tests import structural_tests
-from .timeseries_metrics import timeseries_metrics
 
-__all__ = ["classification_tests", "columnar_tests", "regression_tests", "structural_tests",
-           "timeseries_metrics"]
+
+__all__ = ["classification_tests", "columnar_tests", "regression_tests", "structural_tests"]
