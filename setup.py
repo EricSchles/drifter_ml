8645ec69443e4e7f224e8edf5ec95ff4da54dfa4
Eric Schles <ericschles@gmail.com>
2019-03-27 22:48:54 -0400

    adding setup new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="drifter_ml",
-    version="0.9",
+    version="0.14",
     description="Testing for models confirming to the scikit-learn api",
     long_description=README,
     long_description_content_type="text/markdown",
