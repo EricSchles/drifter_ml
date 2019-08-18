309dbc62f13256e3d49fa04c8569b1120d3d6bd3
Eric Schles <ericschles@gmail.com>
2019-08-17 22:34:15 -0400

    some advice

--- drifter_ml/classification_tests/classification_tests.py
+++ drifter_ml/classification_tests/classification_tests.py
@@ -333,7 +333,8 @@ class ClassificationTests(FixedClassificationMetrics):
     * cross_val_lower_boundary : each fold must be above the lower boundary
     * lower_boundary_per_class : each class must be above a given lower boundary
     The lower boundary per class can be different
-    * cross_val_anomaly_detection : 
+    * cross_val_anomaly_detection : the score for each fold must
+    have a deviance from the average below a set tolerance
     * cross_val_per_class_anomaly_detection : the score for each class 
     for each fold must have a deviance from the average below a set tolerance
     
